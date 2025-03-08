f193193665484ed2e56d806317a6217eba78483c
whiz <kdelgado80491@ufide.ac.cr>
2025-03-08 12:25:29 +0200

    Incorporacion de foreign keys y plantilla basica para poder comprobar navegacion

--- src/main/java/com/proyecto_gym/repository/PreguntaFrecuenteRepository.java
+++ src/main/java/com/proyecto_gym/repository/PreguntaFrecuenteRepository.java
@@ -1,9 +1,7 @@
 package com.proyecto_gym.repository;
 
-
 import com.proyecto_gym.domain.PreguntaFrecuente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PreguntaFrecuenteRepository extends JpaRepository<PreguntaFrecuente, Long> {
-
+public interface PreguntaFrecuenteRepository extends JpaRepository<PreguntaFrecuente, Integer> {
 }
