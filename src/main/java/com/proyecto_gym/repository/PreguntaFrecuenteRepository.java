a4bfcf06cf9756b1838cb9d420c780ed6282c761
whiz <kdelgado80491@ufide.ac.cr>
2025-03-10 22:55:36 +0200

    Removed files that were not mine (Kevin)

--- src/main/java/com/proyecto_gym/repository/PreguntaFrecuenteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto_gym.repository;
-
-import com.proyecto_gym.domain.PreguntaFrecuente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreguntaFrecuenteRepository extends JpaRepository<PreguntaFrecuente, Integer> {
-}
