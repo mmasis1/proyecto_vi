3586628d3180c37d09905b5578399af205ad527c
whiz <kdelgado80491@ufide.ac.cr>
2025-03-11 21:25:54 +0200

    CRUD Categoria W

---
+++ src/main/java/com/proyecto_gym/repository/MetodoPagoRepository.java
@@ -0,0 +1,9 @@
+package com.proyecto_gym.repository;
+
+import com.proyecto_gym.domain.Categoria;
+import com.proyecto_gym.domain.MetodoPago;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MetodoPagoRepository extends JpaRepository<MetodoPago, Integer> {
+}
